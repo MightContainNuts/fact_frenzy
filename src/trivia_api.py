4c8ab6462a0ca50f7338e03a7c157ad2fed96c79
MightContainNuts <mightcontainnuts@icloud.com>
2025-01-14 07:20:16 +0100

    f_02 finished and tests added

--- src/trivia_api.py
+++ src/trivia_api.py
@@ -1,10 +1,16 @@
 import random
 import requests
 from src.logger import setup_logger
+from typing import Tuple
 
-TriviaDict = dict[str : list[dict[str:str]]]  # noqa: E203
+Question = str
+Answers = list[str]
+CorrectAnswer = str
+QuestionValue = int
 TriviaItem = dict[str : [str | list[str]]]  # noqa: E203
-QADict = dict[str : list[str]]  # noqa: E203
+TriviaList = list[TriviaItem]
+QATuple = Tuple[Question, Answers, CorrectAnswer, QuestionValue]  # noqa: E203
+QAList = list[QATuple]  # noqa: E203# noqa: E203
 
 
 class QAStorage:
@@ -21,7 +27,7 @@ class QAStorage:
         self.types = types
         self.categories = categories
         self.limit = limit
-        self.trivia_storage: TriviaDict = {}
+        self.qa_storage: QAList = []
 
         self.logger = setup_logger(__name__)
         self.logger.info(
@@ -39,7 +45,7 @@ class QAStorage:
         :rtype:
         """
         try:
-            self._create_trivia_storage()
+            self._populate_trivia_storage()
         except Exception as e:
             self.logger.error("Error during resource initialization: %s", e)
             raise
@@ -54,7 +60,7 @@ class QAStorage:
         self.logger.info("Exiting context and cleaning up resources.")
         return exc_type is None
 
-    def select_question_from_trivia_store(self, difficulty):
+    def select_question_from_trivia_list(self) -> QATuple:
         """
         select a question from the dictionary based on difficulty
         return tuple with question, right answer and value
@@ -63,44 +69,55 @@ class QAStorage:
         :return:
         :rtype:
         """
-        random_question = self._get_random_qa(difficulty)
+        random_question = self._get_random_qa()
+        return random_question if random_question else None
 
-        question = random_question["question"]["text"]
-
-        correct_answer = self._get_correct_answer_for_selected_question(
-            random_question
-        )
-
-        answers = self._create_list_random_answers(random_question)
-
-        question_value = self._calculate_points_for_question(random_question)
-
-        return question, answers, correct_answer, question_value
-
-    def _create_trivia_storage(self):
+    def _get_random_qa(self) -> QATuple:
         """
-        get 10 questions of each difficulty and add them to a storage
+        geta random Question and Answers from the trivia dict
+        and return one. Remove the chosen one from the trivia dict
         :return:
         :rtype:
         """
+        if len(self.qa_storage):
+            random_question = random.choice(self.qa_storage)
+            self.qa_storage.remove(random_question)
+            self.logger.info("random question picked %s", random_question)
+            return random_question
+
+    # populate trivia storage - happens on class instance
+    def _populate_trivia_storage(self) -> QAList:
+        """
+        get 3 questions of each difficulty and add them to a storage
+        :return:
+        :rtype:
+        """
+
         difficulties = ["easy", "medium", "hard"]
         for difficulty in difficulties:
-            self.trivia_storage[difficulty] = self._fetch_trivia_questions(
+            trivia_temp_storage: TriviaList = self._fetch_trivia_dict(
                 difficulty
             )
             self.logger.info(f"Questions added for {difficulty} class")
+            for trivia_item in trivia_temp_storage:
+                qa_item: QATuple = self._create_qa_tuple(trivia_item)
+                self.qa_storage.append(qa_item)
+        random.shuffle(self.qa_storage)
+        return self.qa_storage
 
-    def _fetch_trivia_questions(self, difficulty) -> TriviaDict:
+    def _fetch_trivia_dict(self, difficulty) -> TriviaList:
         """
         connect to api and retrieve trivia
         :return:
+
         :rtype:
         """
-        question_storage = {}
+        trivia_dict: TriviaList = {}
+        LIMIT = 3
         PATH = "https://the-trivia-api.com/v2/questions/?"
         difficulty = f"difficulties={difficulty}"
         categories = f"&categories={self.categories}"
-        limit = f"&limit={self.limit}"
+        limit = f"&limit={LIMIT}"
 
         URL = PATH + difficulty + categories + limit
         try:
@@ -109,43 +126,74 @@ class QAStorage:
                 self.logger.info(
                     f"Connected to api, status code: {response.status_code}"
                 )
-                question_storage = response.json()
+                trivia_dict = response.json()
                 self.logger.info("converting response to json ")
         except requests.exceptions.RequestException as e:
             self.logger.error("Exception caught by requests %s", e)
             return []
-        return question_storage
+        return trivia_dict
+
+    # create qa tuple from parsed trivia item
+    def _create_qa_tuple(self, trivia_item: TriviaItem) -> QATuple:
+        question: Question = self._parse_question(trivia_item)
+        correct_answer: CorrectAnswer = self._parse_correct_answer(trivia_item)
+        incorrect_answers: Answers = self._parse_incorrect_answers(trivia_item)
+        answers: Answers = self._create_list_random_answers(
+            incorrect_answers, correct_answer
+        )
+        question_value: QuestionValue = self._calculate_points_for_question(
+            trivia_item
+        )
+        return question, answers, correct_answer, question_value
+
+    def _parse_question(self, trivia_item: TriviaItem) -> Question:
+        """
+        parse question from qa dict
+        :param question:
+        :type question:
+        :return:
+        :rtype:
+        """
+        return trivia_item["question"]["text"]
+
+    def _parse_correct_answer(self, trivia_item: TriviaItem) -> CorrectAnswer:
+        """
+        get eh correct answer for the selected question
+        :param selected_question:
+        :type selected_question:
+        :return:
+        :rtype:
+        """
+        self.logger.info("Correct answer for question parsed")
+        return trivia_item["correctAnswer"]
+
+    def _parse_incorrect_answers(self, trivia_item: TriviaItem) -> Answers:
+        """
+        parse list of false answers from trivia item
+        :param trivia_item:
+        :type trivia_item:
+        :return:
+        :rtype:
+        """
+        return trivia_item["incorrectAnswers"]
 
     def _create_list_random_answers(
-        self, selected_question: dict
-    ) -> TriviaItem:  # noqa E501
+        self, incorrect_answers: Answers, correct_answer: CorrectAnswer
+    ) -> Answers:  # noqa E501
         """
         create a list of random placed answers based on json
         :return:
         :rtype:
         """
-        correct_answer = selected_question["correctAnswer"]
-        answers = selected_question["incorrectAnswers"]
-        answers.append(correct_answer)
+        incorrect_answers.append(correct_answer)
+        answers = incorrect_answers
         random.shuffle(answers)
         self.logger.info("List of answers created and shuffled %s", answers)
         return answers
 
-    def _get_random_qa(self, difficulty) -> QADict:
-        """
-        geta random Question and Answers from the trivia dict
-        and return one. Remove the chosen one from the trivia dict
-        :return:
-        :rtype:
-        """
-        random_question = random.choice(self.trivia_storage[difficulty])
-        self.trivia_storage[difficulty].remove(random_question)
-        self.logger.info(
-            f"random question picked based on {difficulty} %s", random_question
-        )
-        return random_question
-
-    def _calculate_points_for_question(self, selected_question) -> int:
+    def _calculate_points_for_question(
+        self, trivia_item: TriviaItem
+    ) -> QuestionValue:
         """
         calculater points based on difficulty
         :return:
@@ -156,23 +204,10 @@ class QAStorage:
             "medium": 2,
             "hard": 3,
         }
-        difficulty = selected_question["difficulty"]
+        difficulty = trivia_item["difficulty"]
         question_value = points[difficulty]
         self.logger.info(
             f"Points calculated for question {difficulty} : {points} %s",
-            selected_question,
+            trivia_item,
         )
         return question_value
-
-    def _get_correct_answer_for_selected_question(
-        self, selected_question
-    ) -> str:
-        """
-        get eh correct answer for the selected question
-        :param selected_question:
-        :type selected_question:
-        :return:
-        :rtype:
-        """
-        self.logger.info("Correct answer for question parsed")
-        return selected_question["correctAnswer"]
--- tests/test_trivia_api.py
+++ tests/test_trivia_api.py
@@ -1,31 +1,208 @@
-from src.trivia_api import QAStorage
+from src.trivia_api import (
+    QAStorage,
+    QAList,
+    QATuple,
+    TriviaItem,
+    TriviaList,
+    Answers,
+    CorrectAnswer,
+)
 import pytest
 
 
 class TestTriviaRetrieval:
+
+    test_trivia_item: TriviaItem = {
+        "category": "science",
+        "id": "6244374e746187c5e7be9345",
+        "correctAnswer": "Correct_Ans!",
+        "incorrectAnswers": [
+            "Incorrect_Ans_1",
+            "Incorrect_Ans_2",
+            "Incorrect_Ans_3",
+        ],
+        "question": {"text": "Question?"},
+        "tags": ["science"],
+        "type": "text_choice",
+        "difficulty": "hard",
+        "regions": [],
+        "isNiche": False,
+    }
+    test_trivia_list: TriviaList = [
+        {
+            "category": "science",
+            "id": "6244374e746187c5e7be9345",
+            "correctAnswer": "Correct_Ans1!",
+            "incorrectAnswers": [
+                "Incorrect_Ans_1.1",
+                "Incorrect_Ans_1.2",
+                "Incorrect_Ans_1.3",
+            ],
+            "question": {"text": "Question1?"},
+            "tags": ["science"],
+            "type": "text_choice",
+            "difficulty": "hard",
+            "regions": [],
+            "isNiche": False,
+        },
+        {
+            "category": "science",
+            "id": "6244374e746187c5e7be9345",
+            "correctAnswer": "Correct_Ans2",
+            "incorrectAnswers": [
+                "Incorrect_Ans_2.1",
+                "Incorrect_Ans_2.2",
+                "Incorrect_Ans_2.3",
+            ],
+            "question": {"text": "Question2?"},
+            "tags": ["science"],
+            "type": "text_choice",
+            "difficulty": "medium",
+            "regions": [],
+            "isNiche": False,
+        },
+        {
+            "category": "science",
+            "id": "6244374e746187c5e7be9345",
+            "correctAnswer": "Correct_Ans3!",
+            "incorrectAnswers": [
+                "Incorrect_Ans_3.1",
+                "Incorrect_Ans_3.2",
+                "Incorrect_Ans_3.3",
+            ],
+            "question": {"text": "Question3?"},
+            "tags": ["science"],
+            "type": "text_choice",
+            "difficulty": "hard",
+            "regions": [],
+            "isNiche": False,
+        },
+    ]
+    test_data_storage: QAList = [
+        (
+            "Question_1",
+            [
+                "Incorrect_Ans_1.1",
+                "Incorrect_Ans_1.2",
+                "Incorrect_Ans_1.3",
+                "Correct_Ans_1",
+            ],
+            "Correct_Ans_1",
+            1,
+        ),
+        (
+            "Question_2",
+            [
+                "Incorrect_Ans_2.1",
+                "Incorrect_Ans_2.2",
+                "Correct_Ans_2" "Incorrect_Ans_2.3",
+            ],
+            "Correct_Ans_2",
+            2,
+        ),
+        (
+            "Question_3",
+            [
+                "Incorrect_Ans_3.1",
+                "Correct_Ans_3" "Incorrect_Ans_3.2",
+                "Incorrect_Ans_3.3",
+            ],
+            "Correct_Ans_3",
+            2,
+        ),
+    ]
+
     @pytest.fixture
     def test_instance(self):
         with QAStorage() as test_inst:
             return test_inst
 
-    @pytest.fixture
-    def test_data_from_api(self, test_instance):
-        test_json = test_instance._fetch_trivia_questions
-        assert test_json
-        return test_instance._fetch_trivia_questions("easy")
+    def test_trivia_dict(self, test_instance):
+        test_qa_dict = test_instance._fetch_trivia_dict("easy")
+        assert test_qa_dict
+        assert len(test_qa_dict) == 3
+        for qa in test_qa_dict:
+            assert qa["difficulty"] == "easy"
 
-    @pytest.fixture()
-    def test_get_random_qa(self, test_instance, test_data_from_api):
-        assert len(test_instance.trivia_storage) == 3
-        test_instance._get_random_qa("easy")
-        assert len(test_instance.trivia_storage) == 3
-        return test_instance._get_random_qa("easy")
+    def test_parse_question(self, test_instance):
+        test_question = test_instance._parse_question(self.test_trivia_item)
+        assert isinstance(test_question, str)
+        assert test_question == self.test_trivia_item["question"]["text"]
+        assert test_question == "Question?"
 
-    def test_create_list_random_answers(
-        self, test_get_random_qa, test_instance
-    ):
-        test_answers = test_instance._create_list_random_answers(
-            test_get_random_qa
+    def test_parse_correct_answer(self, test_instance):
+        test_correct_answer: CorrectAnswer = (
+            test_instance._parse_correct_answer(self.test_trivia_item)
         )
-        assert len(test_answers) == 4
-        assert isinstance(test_answers, list)
+        assert isinstance(test_correct_answer, str)
+        assert test_correct_answer == self.test_trivia_item["correctAnswer"]
+        assert test_correct_answer == "Correct_Ans!"
+
+    def test_parse_incorrect_answers(self, test_instance):
+        test_incorrect_answers: Answers = (
+            test_instance._parse_incorrect_answers(self.test_trivia_item)
+        )
+        assert isinstance(test_incorrect_answers, list)
+        assert len(test_incorrect_answers) == 3
+
+    def test_create_list_random_answer(self, test_instance):
+        correct_answer = test_instance._parse_correct_answer(
+            self.test_trivia_item
+        )
+        incorrect_answers = test_instance._parse_incorrect_answers(
+            self.test_trivia_item
+        )
+        test_list: QAList = test_instance._create_list_random_answers(
+            incorrect_answers, correct_answer
+        )
+        assert isinstance(test_list, list)
+        assert len(test_list) == 4
+        expected_answers = [
+            "Incorrect_Ans_1",
+            "Incorrect_Ans_2",
+            "Incorrect_Ans_3",
+            "Correct_Ans!",
+        ]
+        print(f"Expected answers: {expected_answers}")
+        print(f"Generated test list: {test_list}")
+        assert all(answer in test_list for answer in expected_answers)
+        assert test_list != [
+            "Incorrect_Ans_1",
+            "Incorrect_Ans_2",
+            "Incorrect_Ans_3",
+            "Correct_Ans!",
+        ]
+
+    def test_calculate_points_for_question(self, test_instance):
+        test_points = test_instance._calculate_points_for_question(
+            self.test_trivia_item
+        )
+        assert isinstance(test_points, int)
+        assert test_points == 3
+
+    def test_create_qa_tuple(self, test_instance):
+        qa_tuple: QATuple = test_instance._create_qa_tuple(
+            self.test_trivia_item
+        )
+        assert isinstance(qa_tuple, tuple)
+        assert len(qa_tuple) == 4
+        assert isinstance(qa_tuple[0], str)
+        assert isinstance(qa_tuple[1], list) and all(
+            isinstance(ans, str) for ans in qa_tuple[1]
+        )
+        assert isinstance(qa_tuple[2], str)
+        assert isinstance(qa_tuple[3], int)
+
+    def test_get_random_qa(self, test_instance):
+        test_instance.qa_storage = self.test_data_storage
+        assert len(test_instance.qa_storage) == 3
+        test_instance._get_random_qa()
+        assert len(test_instance.qa_storage) == 2
+
+    def test_select_question_from_trivia_list(self, test_instance):
+        test_instance.qa_storage = self.test_data_storage
+        selected_qa: TriviaItem = (
+            test_instance.select_question_from_trivia_list()
+        )
+        assert isinstance(selected_qa, tuple)
+        assert len(selected_qa) == 4
